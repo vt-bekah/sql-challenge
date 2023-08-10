c6ea459c792802eff569539e56d1965bbe163a01
vt-bekah <vt_bekah@hotmail.com>
2023-08-10 18:28:57 -0500

    Adding ERD files exported from pgAdmin4, updated ChallengeSolution.sql so list headers were more readable; added new files and getting started info within README

--- EmployeeSQL/ChallaengeSolution.sql
+++ EmployeeSQL/ChallaengeSolution.sql
@@ -1,45 +1,78 @@
--- Import csv files (clicking through options in Postgres GUI) and verify data imports as expected
+-- Import csv files (clicking through options in Postgres GUI) and verify data imports as expected:
+
+-- Import from EmployeeSQL\data\titles.csv to titles table
 SELECT * from titles;
+-- Import from EmployeeSQL\data\departments.csv to departments table
 SELECT * from departments;
+-- Import from EmployeeSQL\data\employees.csv to employees table
 SELECT * from employees;
+-- Import from EmployeeSQL\data\salaries.csv to salaries table
 SELECT * from salaries;
+-- Import from EmployeeSQL\data\dept_emp.csv to dept_emp table
 SELECT * from dept_emp;
+-- Import from EmployeeSQL\data\.csv to dept_manager table
 SELECT * from dept_manager;
 
+-- Complete Data Analysis per Instructions in BCS/Canvas
+
 -- List the employee number, last name, first name, sex, and salary of each employee.
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+SELECT 
+	e.emp_no AS "Employee Number", 
+	e.last_name AS "Employee Last Name", 
+	e.first_name AS "Employee First Name", 
+	e.sex AS "Employee Sex", 
+	s.salary AS "Employee Salary"
 FROM employees AS e
 JOIN salaries AS s ON (e.emp_no = s.emp_no);
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
-SELECT e.first_name, e.last_name, e.hire_date
+SELECT 
+	e.first_name AS "Employee First Name", 
+	e.last_name AS "Employee Last Name", 
+	e.hire_date AS "Employee Hire Date"
 FROM employees AS e
 WHERE (EXTRACT(YEAR FROM hire_date)) = 1986;
 
 -- List the manager of each department along with their 
 -- department number, department name, employee number, last name, and first name.
-SELECT d.dept_no, d.dept_name, d_m.emp_no, e.last_name, e.first_name
+SELECT 
+	d.dept_no AS "Department No.", 
+	d.dept_name AS "Department Name", 
+	d_m.emp_no AS "Employee Number", 
+	e.last_name AS "Employee Last Name", 
+	e.first_name AS "Employee First Name"
 FROM departments AS d
 JOIN dept_manager AS d_m ON (d.dept_no=d_m.dept_no)
 JOIN employees AS e ON (d_m.emp_no=e.emp_no);
 
 -- List the department number for each employee along with that 
 -- employee’s employee number, last name, first name, and department name.
-SELECT d.dept_no, d_e.emp_no, e.last_name, e.first_name, d.dept_name
+SELECT 
+	d.dept_no AS "Department No.", 
+	d_e.emp_no AS "Employee Number", 
+	e.last_name AS "Employee Last Name", 
+	e.first_name AS "Employee First Name", 
+	d.dept_name AS "Department Name"
 FROM departments AS d
 JOIN dept_emp AS d_e ON (d.dept_no=d_e.dept_no)
 JOIN employees AS e ON (d_e.emp_no=e.emp_no);
 
 -- List first name, last name, and sex of each employee 
 -- whose first name is Hercules and whose last name begins with the letter B.
-SELECT first_name, last_name, sex
+SELECT 
+	first_name AS "Employee First Name", 
+	last_name AS "Employee Last Name", 
+	sex AS "Employee Sex"
 FROM employees
 WHERE first_name='Hercules' AND last_name LIKE 'B%';
 
 
 -- List each employee in the Sales department, 
 -- including their employee number, last name, and first name.
-SELECT emp_no, last_name, first_name
+SELECT 
+	emp_no AS "Employee Number", 
+	last_name AS "Employee Last Name", 
+	first_name AS "Employee First Name"
 FROM employees
 WHERE emp_no IN
 (
@@ -53,7 +86,11 @@ WHERE emp_no IN
 
 -- List each employee in the Sales and Development departments, 
 -- including their employee number, last name, first name, and department name.
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+SELECT 
+	e.emp_no AS "Employee Number", 
+	e.last_name AS "Employee Last Name", 
+	e.first_name AS "Employee First Name", 
+	d.dept_name AS "Department Name"
 FROM employees AS e
 JOIN dept_emp AS d_e ON (e.emp_no=d_e.emp_no)
 JOIN departments AS d ON (d_e.dept_no=d.dept_no)
@@ -62,7 +99,9 @@ WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
 -- List the frequency counts, in descending order, 
 -- of all the employee last names (that is, how many employees share each last name).
-SELECT last_name, COUNT(last_name) AS "Counts"
+SELECT 
+	last_name AS "Employee Last Name", 
+	COUNT(last_name) AS "Last Name Count"
 FROM employees
 GROUP BY last_name;
 
--- README.md
+++ README.md
@@ -2,23 +2,25 @@
 This repository contains challenge files for UT DAV Bootcamp Module 9 SQL
 
 # File Notes
-* EmployeeSQL folder contains all the files required to complete the challenge as indicated by the Instructions below.
+* EmployeeSQL folder contains all the files required to complete the challenge as indicated by the Challenge Instructions below.
    * data folder contains all the csv files downloaded from BCS/Canvas that were imported into the SQL tables
       **Note** Some employees.first_names were "TRUE" but the instructions did not address this oddity; TA advised to treat these as a name.
    * resources folder contains all the schema files created in the QuickDBD tool linked in the references:
       * SchemaDiagram_QuickDBD.png is a picture of the ERD created in Quick DBD for the tables created in this challenge (Create an image file of your ERD.)
       * Schema_QuickDBD.pdf is the pdf of the text input to the QuickDBD tool to create the ERD
       * Schema_QuickDBD.sql is the file used to create each table within PostgreSQL (Create a .sql file of your table schemata.)
-   * ChallengeSolutions.sql contains queries to view the tables after importing the csv files as well as the data analysis requested within the instructions (Create a .sql file of your queries.)
+   * ChallengeSolutions.sql contains queries to view the tables after importing the csv files as well as the data analysis requested within the Challenge Instructions (Create a .sql file of your queries.)
+   * ERD_PostgreSQL_pgAdmin4.pgerd is the ERD export from PostgreSQL pgAdmin4 after creating the tables using Schema_QuickDBD.sql
+   * ERD_PostgreSQL_pgAdmin4.png is a snapshot of the ERD view from PostgreSQL pgAdmin4 after creating the tables using Schema_QuickDBD.sql
 * Starter_Code folder contains the files provided in BCS/Canvas for completing the challenge.
    
 
-
 # References
 The following references were used in creating the solution within the PyCitySchools folder:
- * http://www.quickdatabasediagrams.com/ was used to create the ERD as well as SQL file to be used in PostgreSQL
+ * http://www.quickdatabasediagrams.com/ was used to create the initial ERD as well as SQL file to be used in PostgreSQL to create the tables used in this challenge.
 
-# Instructions
+
+# Challenge Instructions
 
 **Data Modeling**
 Inspect the CSV files, and then sketch an Entity Relationship Diagram of the tables. To create the sketch, feel free to use a tool like QuickDBD (linked in references) to an external site.
@@ -39,3 +41,16 @@ Inspect the CSV files, and then sketch an Entity Relationship Diagram of the tab
 6. List each employee in the Sales department, including their employee number, last name, and first name.
 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+# Getting Started
+
+## Prerequisites
+You must have PostgreSQL and pgAdmin4 installed
+
+## Cloning Repo
+$ git clone https://github.com/vt-bekah/sql-challenge.git
+$ cd sql-challenge
+
+# Built With
+* PostgreSQL version 14.9 for Windows x86-64 https://www.enterprisedb.com/downloads/postgres-postgresql-downloads 
+* pgAdmin 4 version 7.5  https://www.postgresql.org/ftp/pgadmin/pgadmin4/v7.5/windows/ 
